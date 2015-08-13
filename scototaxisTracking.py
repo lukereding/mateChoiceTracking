6b0ec0864cae54b1eb2e8512a200b23de9ee70fa
luke reding <lukereding@gmail.com>
2015-08-13 15:27:02 -0500

    Create scototaxisTracking.py

---
+++ scototaxisTracking.py
@@ -0,0 +1,22 @@
+"""
+this function calculates that percent of time a fish spends on the white side of a black/white tank
+
+started 13 August 2015, luke reding
+"""
+
+import cv2, sys, csv
+import numpy as np
+
+# read video file
+
+# in a loop, query frames
+
+# use same function as in normal tracking code to get photo of empty tank
+
+# for each frame, do blurring followed by background subtraction, thresholding at some level TBD
+
+# order the contours by length and take the longest one
+
+# write it to a csv file
+
+# done
