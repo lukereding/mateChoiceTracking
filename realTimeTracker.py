de3f38019c872dd897da17a25272086415bcb461
lukereding <lukereding@gmail.com>
2015-08-25 17:30:34 -0500

    kept working throgh it. made it basically functional for real time tracking

--- realTimeTracker.py
+++ realTimeTracker.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env
 import numpy as np
-import cv2, csv, os, re, sys
+import cv2, csv, os, re, sys, time
 import argparse
 
 
 '''
 started 25 August 2015
+
+start this script 10 seconds before you actually want it to start to allow time for inializing
+it will automatically start after 10 seconds
+
+I've something goofy in that I've sort of artificially lowered the resolution of the videos to maintain compatibility with previous code using resize()
+this could be changed in future versions of this code 
 
 this here is a working copy of a python script I hope to use to accomplish the following:
 -- track fish in mate choice tank in real time
@@ -16,94 +22,68 @@ this here is a working copy of a python script I hope to use to accomplish the f
 or if the fish needs to be re-tested
 
 
-
 '''
 
 
 # initialize some constants, lists, csv writer
 # construct the argument parse and parse the arguments
+start_time = time.time()
+end_time = start_time + 10
 ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--inputVideo", help = "path to the video")
+ap.add_argument("-i", "--inputCamera", help = "integer that represents which usb input the camera is. typically 0 or 1")
+ap.add_argument("-n", "--name", help = "name of the female. e.g. Jill")
 args = vars(ap.parse_args())
 lower = np.array([0,0,0])
 upper = np.array([255,255,20])
 counter = 0
 
-# set up the video capture to the video that was the argument to the script
-cap = cv2.VideoCapture(args["inputVideo"])
-
 # output to csv file where the results will be written
-name = re.split('[/.]', args["inputVideo"], flags=re.IGNORECASE)[-2]
-name = name + ".csv"
+name = args["name"] + ".csv"
 print "name of csv file: " + str(name)
-
 myfile = open(name,'wb')
 csv_writer = csv.writer(myfile, quoting=csv.QUOTE_NONE)
 csv_writer.writerow(("x","y","frame"))
 
-# test to make sure the video dimensions are the right size
-_,test = cap.read()
-if test.shape[0] != 720:
-	print "*************\nnote that this script is meant for videos that are 1280x720 pixels. It looks like your video isn't. You can either re-scale using ffmpeg or alter this code (espeically the masking step)\n********"
+# for drawing the rectangle around the tank at the beginning of the trial
+drawing = False # true if mouse is pressed
+ix,iy = -1,-1
 
+# print python version
+print sys.version 
 
 ######################
 # declare some functions:####
 #####################################
 
-# make sure the camera is working. if not, return false
-def checkCamera(x):
-	cap = cv2.VideoCapture(x)
-	ret, frame = cap.read()
-	
-	if ret == False:
-		return(False)
-	else:
-		cap.close()
-		return(True)
+# mouse callback function; draws the rectangle
+def drawRectangle(event,x,y,flags,param):
+    global ix,iy,drawing
+    if event == cv2.EVENT_LBUTTONDOWN:
+        drawing = True
+        ix,iy = x,y
+
+    elif event == cv2.EVENT_LBUTTONUP:
+        drawing = False
+        cv2.rectangle(frame,(ix,iy),(x,y),(0,0,255),5)
+        # globally define the boundaries of the tank
+        global top_bound, left_bound, right_bound, lower_bound
+        top_bound, left_bound, right_bound, lower_bound = iy, ix, x, y
+        print "Rectangle bounds: "
+        print top_bound, left_bound, right_bound, lower_bound
 
 # converts a frame to HSV, blurs it, masks it to only get the tank by itself
 def convertToHSV(frame):
+	# blur image to make color uniform
 	blurred = cv2.blur(frame,(7,7))
+	# conver to hsv
 	hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+	# apply mask to get rid of stuff outside the tank
+	
+	### TO DO: change these mask bounds according the circle drawn
 	mask = np.zeros((720, 1280, 3),np.uint8)
 	mask[150:600,150:1170] = hsv[150:600,150:1170]
 	return mask
 
-
-# function to determine where the fish started in the video
-def findStartingPoint(video,numberOfFrames):
-	# the function will go numberOfFrames frames ahead in the video and do a subtraction
-	# the hope is that the fish will have moved at some point during this time
-	count = 0
-	# I don't know a better way to jump ahead so many frames
-	while count <= numberOfFrames:
-		# read in the frame for each tick of the loop
-		ret,frame = video.read()
-		# grab a frame from the middle. Assume the screens have turned on by then
-		if count == numberOfFrames/2:
-			hsv_middle = convertToHSV(frame)
-		if count == numberOfFrames:
-			# grab the last frame in the numberOfFrames window
-			hsv_end = convertToHSV(frame)
-		count += 1
-		if count == 1:
-			print "initializing"
-		print " " * (20-(count%20)) + "." * (count%20) + "." +  "." * (count%20)
-		
-	# now we have masked HSV photos from the beginning, middle, and end of the initialization period
-	# now we have to do some funky subtraction to get rid of the signal that results from the screen being turn on
-	difference2 = cv2.subtract(hsv_end,hsv_initial)
-	difference1 = cv2.subtract(hsv_end,hsv_middle)
-	difference3 = cv2.subtract(difference1,difference2)
-	
-	# now we have what we want
-	thresh = cv2.inRange(difference3,np.array([0,0,0]),np.array([255,255,25]))
-	# need to invert the colors or else we run into trouble on our call to cv2.findContours
-	invert = cv2.bitwise_not(thresh)
-	startingPoint = returnLargeContour(invert)
-	return(startingPoint)
-	
 	
 # returns centroid from largest contour from a binary image
 def returnLargeContour(frame):
@@ -166,24 +146,60 @@ def getBackgroundImage(vid,numFrames):
 			print "detecting background -- on frame " + str(i) + " of " + str(numFrames)
 	return final
 
-###################
+#########################
 ## end function declarations ####
 ###################################################
 
-# check to make sure the camera is hooked up and works
-# if not, exit the program
-working = checkCamera(0)
-if working == False:
-	sys.exit("\n\ncamera doesn't appear to be hooked up or change argument to checkCamera function to 1")
+# print input argument to the terminal
+print "input: " + str(args["inputCamera"])
 
+captureNumber = args["inputCamera"]
+# this is a string. If it can be converted to an integer, then convert it
+try:
+	int(float(captureNumber))
+	captureNumber = int(captureNumber)
+except:
+	pass
+
+# set up the video capture to the video that was the argument to the script
+cap = cv2.VideoCapture(captureNumber)
+
+# set the right resolution
+cap.set(3,1280)
+cap.set(4,720)
+
+print "camera feed dimensions: " + str(cap.get(3)) + " x " + str(cap.get(4))
 
 # grab the first frame
 ret,frame = cap.read()
 hsv_initial = convertToHSV(frame)
 
+# loop to have the user draw the rectangle
+while(True):
+	cv2.namedWindow('tank')
+	cv2.setMouseCallback('tank',drawRectangle)
+	cv2.imshow('tank',frame)
+	k = cv2.waitKey(1) & 0xFF
+	if k == 27:
+		cv2.destroyAllWindows()
+		break
+
 # could also try:
 # background = getBackgroundImage(cap,500)
 # background = convertToHSV(background)
+
+
+# keep a list of coordinates of the fish.
+# the idea is, for the purposes of this code, if we can't ID the fish we assume it's stopped
+# the csv file we save will have NAs for these frames instead
+# in R, I then go back and interpolate the missing frames
+# it would be nice to have a python function at the end of this script that would do that for me, actually
+
+# initiating with the center of the tank in case tracker can't find fish initially
+center1 =(right_bound-left_bound)/2
+center2 = (lower_bound-top_bound)/2
+coordinates = [(center1,center2)]
+print coordinates
 
 
 # the main loop
@@ -191,43 +207,55 @@ while(cap.isOpened()):
 
 	print "frame " + str(counter) + "\n\n"
 	
-	if counter == 0:
-		findStartingPoint(cap,100)
-		counter += 1
-		# re-start the video capture
-		cap = cv2.VideoCapture(args["inputVideo"])
-		
+	while(time.time() < end_time):
+		print "starting in " + str(round(end_time - time.time(),1)) + "seconds" 
+		# wait 0.1 seconds
+		time.sleep(0.1)
+	
+	# for timing
+	beginningOfLoop = time.time()
+	
+	ret,frame = cap.read()
+	hsv = convertToHSV(frame)
+	difference = cv2.subtract(hsv_initial,hsv)
+	masked = cv2.inRange(difference,lower,upper)
+	maskedInvert = cv2.bitwise_not(masked)
+
+	# find the centroid of the largest blob
+	center = returnLargeContour(maskedInvert)
+	
+	# if the fish wasn't ID'ed by the tracker, assume it's stopped moving
+	if not center:
+		coordinates.append(coordinates[-1]) 
+	# otherwise add the coordinate to the growing list
 	else:
-		ret,frame = cap.read()
-		hsv = convertToHSV(frame)
-		difference = cv2.subtract(hsv_initial,hsv)
-		masked = cv2.inRange(difference,lower,upper)
-		maskedInvert = cv2.bitwise_not(masked)
-		
-		# find the centroid of the largest blob
-		center = returnLargeContour(maskedInvert)
-		print "Center: " + str(center) + "\n"
-		# draw the centroids on the image
-		if not not center:
-			cv2.circle(frame,center,6,[0,0,255],-1)
-		# to save the frames:
-		#filename = "/Users/lukereding/Desktop/example/" + "{0:05d}".format(counter) + ".jpg"
-		#cv2.imwrite(filename,frame)
-		
-		cv2.imshow('image',frame)
-		cv2.imshow('thresh',masked)
-		cv2.imshow('diff',difference)
-		
-		k = cv2.waitKey(1)
-		if k == 27:
-			break
-		
-		# if the camera 'warms up' at the beginning or the lights from the screen light up at the beginning, changing the light environment of the tank,
-		# uncommenting the following lines can help
-		# the idea here is to re-set the 'initial' image every 150 frames in case there are changes with the light or top of the water reflections
-		if counter % 150 ==0:
-			hsv_initial = hsv
-		
-		counter+=1
+		coordinates.append(center)
+	
+	print "Center: " + str(center) + "\n"
+	# draw the centroids on the image
+	cv2.circle(frame,coordinates[-1],6,[0,0,255],-1)
+	# to save the frames:
+	#filename = "/Users/lukereding/Desktop/example/" + "{0:05d}".format(counter) + ".jpg"
+	#cv2.imwrite(filename,frame)
+
+	cv2.imshow('image',frame)
+	cv2.imshow('thresh',masked)
+	cv2.imshow('diff',difference)
+	
+	# print how long this loop took
+	endOfLoop = time.time()
+	print "time of loop: " + str(round(endOfLoop-beginningOfLoop,4))
+	
+	k = cv2.waitKey(1)
+	if k == 27:
+		break
+
+	# if the camera 'warms up' at the beginning or the lights from the screen light up at the beginning, changing the light environment of the tank,
+	# uncommenting the following lines can help
+	# the idea here is to re-set the 'initial' image every 150 frames in case there are changes with the light or top of the water reflections
+	if counter % 150 ==0:
+		hsv_initial = hsv
+
+	counter+=1
 
 cv2.destroyAllWindows()
