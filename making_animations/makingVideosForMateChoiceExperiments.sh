011ee78b59869826371b69615c72166f1ccd3e87
lukereding <lukereding@gmail.com>
2015-09-11 15:41:13 -0500

    debugged shell script

--- making_animations/makingVideosForMateChoiceExperiments.sh
+++ making_animations/makingVideosForMateChoiceExperiments.sh
@@ -4,20 +4,19 @@
 # for mate choice trials
 # started March 2015
 
-# to future luke: best workflow
+# to future luke: best workflow:
 ## clone this repo.
-## navigate to this folder
+## cd into this directory
 ## add the following ingredients:
-### the two videos of what you want to show the fish
+### four animations. If you're have animations called A.avi and B.avi, you need A_flipped.avi and B_flipped.avi as well where the background in flipped
 ### photo of whatever background you're using, of the same dimensions as the video
-## make this script executable with chmod makingVideosForMateChoiceExperiments.sh
-
-# example of usage
-## ./makingVideosForMateChoiceExperiments.sh nameOfVideo1 nameOfVideo2
+### countdown
+## make this script executable with chmod +x makingVideosForMateChoiceExperiments.sh
+## bash +x makingVideosForMateChoiceExperiments.sh A B # for videos named A.avi, B.avi, A_flipped.avi, B_flipped.avi
 
 
 # some other things:
-### due to my poor bash coding abilities, the animations need to be .avi files and need to be passed as arguments to the script. the file extension should be left off
+### due to my poor bash coding abilities, the animations need to be .avi files and need to be passed as arguments to the script. the file extension should be left off but they must be avi files
 ### the countdown video should have count in it and be a .mp4 file
 
 ##### Blender outputting
@@ -34,56 +33,88 @@
 # I also save a photo of the background without the fish to use for the background
 ############################# 
 
+### variables:
+
+VID1=$1
+VID2=$2
+ALMOST1="$1""_almost.mp4"
+ALMOST2="$2""_almost.mp4"
+FINAL1="$1""_final.mp4"
+FINAL2="$2""_final.mp4"
+LOOPED1="$1""_looped.mp4"
+LOOPED2="$2""_looped.mp4"
+
+echo "$VID1"
+echo "$VID2"
+echo "$ALMOST1"
+echo "$ALMOST2"
+echo "$FINAL1"
+echo "$FINAL2"
+echo "$LOOPED1"
+echo "$LOOPED2"
+
 
 ##### black video
-ffmpeg -framerate 1/300 -i black.jpg -framerate 25 black.mp4
+ffmpeg -framerate 1/300 -i black.jpg -r 25 -y black.mp4
 
 ##### countdown
 # scale the countdown by the resolution of your monitor
 # make 25 fps
 # flip it
-ffmpeg -i Final10Countdown.mp4 -vf -framerate 25 scale=1280:1024 final10MinCountdown.mp4
+ffmpeg -i Final10Countdown.mp4 -r 25 -vf scale=1280:1024 -y final10MinCountdown.mp4
 
 ####### background video
 # if the dimensions of background.jpg are not even numbers, this will throw an error
 # make the video at 25 fps
 # flip it
-ffmpeg -framerate 1/300 -i *.jpg -framerate 25 -vf "hflip,vflip,format=yuv420p" background5min.mp4
+ffmpeg -framerate 1/300 -i [Bb]ack*.jpg -r 25 -y background5minPre.mp4
+ffmpeg -i background5minPre.mp4 -r 25 -vf "hflip,vflip,format=yuv420p" -y background5min.mp4
 
 
 # make inset black video in in background video
-ffmpeg -i black.mp4 -i  background5min.mp4 -filter_complex "nullsrc=size=1280x1024 [base]; [0:v] setpts=PTS-STARTPTS, scale=1280x1024 [black]; [1:v] setpts=PTS-STARTPTS, scale=1280x550 [background]; [base][black] overlay=shortest=1 [tmp1]; [tmp1][background] overlay=shortest=1" -c:v libx264 -y FinalBackground5min.mp4
+ffmpeg -i black.mp4 -i background5min.mp4 -filter_complex "nullsrc=size=1280x1024 [base]; [0:v] setpts=PTS-STARTPTS, scale=1280x1024 [black]; [1:v] setpts=PTS-STARTPTS, scale=1280x550 [background]; [base][black] overlay=shortest=1 [tmp1]; [tmp1][background] overlay=shortest=1" -c:v libx264 -y FinalBackground5min.mp4
 
 
 ######## animations
 # assumes your animations are .avi files and that there are two of them:
 
-VID1=$1
-VID2=$2
-
 ## process first video
 echo "Processing $VID1 video..."
 # get the frame rate, codec right
-ffmpeg -i $VID1.avi -vcodec libx264 -y -r 25 -y $VID1.mp4
+ffmpeg -i "$VID1"".avi" -vcodec libx264 -r 25 -pix_fmt yuv420p -y "$VID1"".mp4"
+ffmpeg -i "$VID1""_flipped.avi" -vcodec libx264 -vf "hflip,format=yuv420p" -r 25 -y "$VID1""_flipped2.mp4"
+# combine the unflipped and flipped animations
+echo "file '"$VID1"".mp4"'" > flipped
+echo "file '"$VID1""_flipped2.mp4"'" >> flipped
+ffmpeg -f concat -i flipped -vcodec copy -y "$VID1""_complete.mp4"
+
 # loop the animation
-for i in {1..20}; do printf "file '%s'\n" $VID1.mp4 >> list.txt; done
-ffmpeg -f concat -i list.txt -c copy -y $VID1_looped.mp4
+rm list.txt
+for i in {1..20}; do printf "file '%s'\n" "$VID1""_complete.mp4" >> list.txt; done
+ffmpeg -f concat -i list.txt -c copy -y "$LOOPED1"
 # trim down to 5 min
-ffmpeg -ss 00:00:00 -t 00:05:00 -i $VID1_looped.mp4 -vf "hflip,vflip,format=yuv420p" $VID1_almost_final.mp4
+ffmpeg -ss 00:00:00 -t 00:05:00 -i "$LOOPED1" -vf "hflip,vflip,format=yuv420p" -y "$ALMOST1"
 # embed in background
-ffmpeg -i background5min.mp4 -i $VID1_almost_final.mp4 -filter_complex "nullsrc=size=1280x1024 [base]; [0:v] setpts=PTS-STARTPTS, scale=1280x1024 [background]; [1:v] setpts=PTS-STARTPTS, scale=1280x550 [fish]; [base][background] overlay=shortest=1 [tmp1]; [tmp1][fish] overlay=shortest=1" -c:v libx264 -y $VID1_final.mp4
+ffmpeg -i black.mp4 -i "$ALMOST1" -filter_complex "nullsrc=size=1280x1024 [base]; [0:v] setpts=PTS-STARTPTS, scale=1280x1024 [background]; [1:v] setpts=PTS-STARTPTS, scale=1280x550 [fish]; [base][background] overlay=shortest=1 [tmp1]; [tmp1][fish] overlay=shortest=1" -c:v libx264 -y "$FINAL1"
 
 ## process second video
-echo "Processing $VID2 video..."
+echo "Processing $VID1 video..."
 # get the frame rate, codec right
-ffmpeg -i $VID2.avi -vcodec libx264 -y -r 25 -y $VID2.mp4
+ffmpeg -i "$VID2"".avi" -vcodec libx264 -r 25 -pix_fmt yuv420p -y "$VID2"".mp4"
+ffmpeg -i "$VID2""_flipped.avi" -vcodec libx264 -vf "hflip,format=yuv420p" -r 25 -y "$VID2""_flipped2.mp4"
+# combine the unflipped and flipped animations
+echo "file '"$VID2"".mp4"'" > flipped
+echo "file '"$VID2""_flipped2.mp4"'" >> flipped
+ffmpeg -f concat -i flipped -vcodec copy -y "$VID2""_complete.mp4"
+
 # loop the animation
-for i in {1..20}; do printf "file '%s'\n" $VID2.mp4 >> list.txt; done
-ffmpeg -f concat -i list.txt -c copy -y $VID2_looped.mp4
+rm list.txt
+for i in {1..20}; do printf "file '%s'\n" "$VID2""_complete.mp4" >> list.txt; done
+ffmpeg -f concat -i list.txt -c copy -y "$LOOPED2"
 # trim down to 5 min
-ffmpeg -ss 00:00:00 -t 00:05:00 -i $VID2_looped.mp4 -vf "hflip,vflip,format=yuv420p" $VID2_almost_final.mp4
+ffmpeg -ss 00:00:00 -t 00:05:00 -i "$LOOPED2" -vf "hflip,vflip,format=yuv420p" -y "$ALMOST2"
 # embed in background
-ffmpeg -i background5min.mp4 -i $VID2_almost_final.mp4 -filter_complex "nullsrc=size=1280x1024 [base]; [0:v] setpts=PTS-STARTPTS, scale=1280x1024 [background]; [1:v] setpts=PTS-STARTPTS, scale=1280x550 [fish]; [base][background] overlay=shortest=1 [tmp1]; [tmp1][fish] overlay=shortest=1" -c:v libx264 -y $VID2_final.mp4
+ffmpeg -i black.mp4 -i "$ALMOST2" -filter_complex "nullsrc=size=1280x1024 [base]; [0:v] setpts=PTS-STARTPTS, scale=1280x1024 [background]; [1:v] setpts=PTS-STARTPTS, scale=1280x550 [fish]; [base][background] overlay=shortest=1 [tmp1]; [tmp1][fish] overlay=shortest=1" -c:v libx264 -y "$FINAL2"
 
 
 ## at this point, you should have all the videos you need. 
@@ -93,30 +124,16 @@ ffmpeg -i background5min.mp4 -i $VID2_almost_final.mp4 -filter_complex "nullsrc=
 
 ## video1:
 echo "file 'final10MinCountdown.mp4'" > vid1
-echo "file 'background5min.mp4'" >> vid1
-echo "file '$VID1_final.mp4'" >> vid1
-echo "file 'background5min.mp4'" >> vid1
-echo "file '$VID2_final.mp4'" >> vid1
+echo "file 'FinalBackground5min.mp4'" >> vid1
+echo "file '$FINAL1'" >> vid1
+echo "file 'FinalBackground5min.mp4'" >> vid1
+echo "file '$FINAL2'" >> vid1
 ffmpeg -f concat -i vid1 -y FinalVideo1.mp4
 
 ## video2:
-echo "file 'final10MinCountdown.mp4'" > vid1
-echo "file 'background5min.mp4'" >> vid1
-echo "file '$VID2_final.mp4'" >> vid1
-echo "file 'background5min.mp4'" >> vid1
-echo "file '$VID1_final.mp4'" >> vid1
-ffmpeg -f concat -i vid1 -y FinalVideo2.mp4
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+echo "file 'final10MinCountdown.mp4'" > vid2
+echo "file 'FinalBackground5min.mp4'" >> vid2
+echo "file '$FINAL2'" >> vid2
+echo "file 'FinalBackground5min.mp4'" >> vid2
+echo "file '$FINAL1'" >> vid2
+ffmpeg -f concat -i vid2 -y FinalVideo2.mp4
