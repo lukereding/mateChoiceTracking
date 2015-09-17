8d8ce99135a7c554d51c162839f488286e5b72cc
lukereding <lukereding@gmail.com>
2015-09-17 09:46:12 -0500

    changed setting on record and track to make compatible with using ms lifecam webcam with ffmpeg 2.6.3

--- recordAndTrack.sh
+++ recordAndTrack.sh
@@ -5,10 +5,13 @@ LENGTH="$2" # in seconds
 FPS="$3"
 
 # optional: sleep during the 10 minute acclimation period
-sleep 600;
+sleep 598;
+
+which ffmpeg
 
 # start ffmpeg
-ffmpeg -f avfoundation -i "Microsoft:none" -q 25 -t "$LENGTH" -vf fps="${FPS}" -vcodec libx264 -y "$NAME"".avi"
+# see https://trac.ffmpeg.org/wiki/EncodingForStreamingSites for explanations of some of the arguments
+ffmpeg -f avfoundation -i "Microsoft:none" -preset superfast -q 30 -crf 30 -maxrate 3000k -bufsize 800k -r "$FPS" -framerate "$FPS" -t "$LENGTH" -vcodec libx264 -y "$NAME"".avi"
 
 
 # wait for it to finish 
@@ -16,4 +19,7 @@ wait
 
 # start tracking
 echo "tracking ${NAME}"
-python realTimeTracker.py -i ${NAME}.avi -n ${NAME} -f ${FPS}
+python realTimeTracker.py -i ${NAME}.avi -n ${NAME} -f ${FPS} >> "$NAME""_log.txt"
+cat "$NAME""_log.txt" | grep "part 2" -A 5
+cat "$NAME""_log.txt" | grep "part 4" -A 5 
+tail -16 "$NAME""_log.txt"
