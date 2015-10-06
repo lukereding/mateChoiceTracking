8b57ef719d4e42b762029916b90c4a086915c345
lukereding <lukereding@gmail.com>
2015-10-06 15:26:06 -0500

    updated ethogram

--- plotting/ethogram.R
+++ plotting/ethogram.R
@@ -1,3 +1,20 @@
+###############
+########## 6 October 2015 ###
+###########################################
+
+# contains R code to plot an ethogram-like representation of where a female was in a tank: right side, left, or neutral
+# the goal is to have some quick and dirty visual representation of what a female is doing in a given trial
+
+## to do:
+### modify so that instead 
+
+# to run: ethogram("/path/to/file.txt, name_of_trial)
+# the /path/to/file.txt files is a newline separated list of sides of the tank where the female was located; each line is a frame
+# each line can take one of three options: neutral, left, or right
+
+# example: ethogram("/Volumes/LPRLABBKP/transitivity_trials/Madeline_Large_vs_Small.txt", name="madeline")
+
+
 # arguments: a vector of things and a starting index
 # it sees what is listed under the starting index and sees whether the same thing is in the next index
 # it keeps doing this until it finds something that doesn't match whatever was under the starting index
@@ -15,22 +32,18 @@ find_clumps <- function(x,start){
   return(i-1)
 }
 
-
-
 draw_rectangle <- function(start_x,end_x,y,offset,color,chunk_length){
-  
-  rect(start_x,y+offset,end_x,y-offset,col=color, border = NA )
-  
+  rect(start_x,y+offset,end_x,y-offset,col=color, lwd=0.5,border=NA)
 }
 
 
-draw_rectangles <- function(sides,i=1,end_i = 100,y_position,size,offset,part=1){
+draw_rectangles <- function(sides,i=1,end_i = 100,y_position,size,offset,part=1,col=c("#482878FF","grey80","#35B779FF")){
   i_initial = i
   
   while(i < end_i){
     new_i <- find_clumps(sides,i)
-    print(c(i, new_i))
-    print(c(i - i_initial,new_i - i_initial))
+    #print(c(i, new_i))
+    #print(c(i - i_initial,new_i - i_initial))
     # for error checking:
     #print ("Press [enter] to continue")
     #number <- scan(n=1)
@@ -39,7 +52,7 @@ draw_rectangles <- function(sides,i=1,end_i = 100,y_position,size,offset,part=1)
  
        # this if else statement is nec. because of the modolo operations in the call to draw_rectangle
     if(new_i %% size >= i %% size){
-      draw_rectangle(i - i_initial,new_i - i_initial,y_position,color = c("#482878FF","white","#35B779FF")[match(sides[i],levels(as.factor(sides)))],chunk_length=size,offset=offset)
+      draw_rectangle(i - i_initial,new_i - i_initial,y_position,color = col[match(sides[i],levels(as.factor(sides)) %>% sort)],chunk_length=size,offset=offset)
       
       # if i = new_i, then behavior only appears for a second, so go to the next frame
       if(new_i == i){
@@ -53,7 +66,7 @@ draw_rectangles <- function(sides,i=1,end_i = 100,y_position,size,offset,part=1)
     else{
       print("end of the line")
       print(c(i %% size, end_i))
-      draw_rectangle(i - i_initial,end_i - i_initial,y_position,color = c("#482878FF","white","#35B779FF")[match(sides[i],levels(as.factor(sides)) %>% sort)],chunk_length=size,offset=offset)
+      draw_rectangle(i - i_initial,end_i - i_initial,y_position,color = col[match(sides[i],levels(as.factor(sides)) %>% sort)],chunk_length=size,offset=offset)
       i <- end_i
     }
   }
@@ -80,20 +93,21 @@ ethogram <- function(path,name){
   plot(1:chunk_length,seq(1,chunk_length,by=1),type="n",bty="n",xlab="time (sec.)",ylab="",yaxt='n',xaxt="n",main=name,asp=0.5)
   axis(1,at=seq(0,3000,by=500),labels=seq(0,3000,by=500)/10)
   
-  off <- bounds[2]*0.03
+  off <- bounds[2]*0.05
   
-  draw_rectangles(sides_vector,i=1,end_i = bounds[2], y_position=bounds[2],size=bounds[2],part=1,offset=off)
+  
+  draw_rectangles(sides_vector,i=1,end_i = bounds[2], y_position=bounds[2],size=bounds[2],part=1,offset=off,col=c("grey60","grey90","grey20"))
   print("drew part 1")
-  draw_rectangles(sides_vector,i=bounds[3],end_i = bounds[4], y_position=bounds[2]*0.75,size=bounds[2],part=2,offset=off)
+  draw_rectangles(sides_vector,i=bounds[3],end_i = bounds[4], y_position=bounds[2]*0.75,size=bounds[2],part=2,offset=off,col=c("#440154FF","grey90","#35B779FF"))
   print("drew part 2")
-  draw_rectangles(sides_vector,i=bounds[5],end_i = bounds[6], y_position=bounds[2]*0.5,size=bounds[2],part=3,offset=off)
+  draw_rectangles(sides_vector,i=bounds[5],end_i = bounds[6], y_position=bounds[2]*0.5,size=bounds[2],part=3,offset=off,col=c("grey60","grey90","grey20"))
   print("drew part 3")
-  draw_rectangles(sides_vector,i=bounds[7],end_i = bounds[8], y_position=bounds[2]*0.25,size=bounds[2],part=4,offset=off)
+  draw_rectangles(sides_vector,i=bounds[7],end_i = bounds[8], y_position=bounds[2]*0.25,size=bounds[2],part=4,offset=off,col=c("#35B779FF","grey90","#440154FF"))
   print("drew part 4")
-  draw_rectangles(sides_vector,i=bounds[9],end_i = bounds[10], y_position=bounds[2]*0,size=bounds[2],part=5,offset=off)
+  draw_rectangles(sides_vector,i=bounds[9],end_i = bounds[10], y_position=bounds[2]*0,size=bounds[2],part=5,offset=off,col=c("grey60","grey90","grey20"))
   print("drew part 5")
   # draw legend
-  legend(x = bounds[2]*0.45, y= bounds[2]*0.55, c("left","right","neutral"), fill=c("#482878FF","#35B779FF","white"),border="black",ncol = 3,bty="o", box.lwd=0.3) 
+  legend(x = bounds[2]*0.45, y= bounds[2]*0.6, c("left","right","neutral","stim1","stim2"), fill=c("grey60","grey20","grey90","#440154FF","#35B779FF"),border=NA,ncol=2,bty="o", box.lwd=0.3) 
   
   
   # label
@@ -107,4 +121,4 @@ ethogram <- function(path,name){
   print(chunks)
   on.exit(par(op))
 }
-
+ethogram("/Volumes/LPRLABBKP/transitivity_trials/Madeline_Large_vs_Small.txt", name="madeline")
