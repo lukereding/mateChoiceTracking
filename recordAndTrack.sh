56df0be1ba17af32b442bf8f57208896d8f0645e
Luke Reding <lukereding@wireless-10-146-148-246.public.utexas.edu>
2015-12-09 18:14:32 -0600

    reverted previous commit (doesn't work); added shebang

--- recordAndTrack.sh
+++ recordAndTrack.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 echo $0
 # read in arguments
 NAME="$1"
@@ -10,7 +12,7 @@ FPS="$3"
 # start ffmpeg
 # note here: the order of the arguments matters. Specifically, the framerate and video size arguments must be given before the -i or the line will throw an error
 # this line was tested > 50 times with the logitech camera
-ffmpeg -f avfoundation -video_size 1280x720 -framerate "$FPS" -i "Micro:none" -crf 28 -vcodec libx264 -t "$LENGTH" "$NAME"".avi"
+ffmpeg -f avfoundation -video_size 1280x720 -framerate "$FPS" -i "Micro:none" -crf 28 -y -vcodec libx264 -t "$LENGTH" "$NAME"".avi"
 
 # to find total frames: (double check this with other videos:
 #ffprobe -select_streams v -show_streams test.avi 2>/dev/null | grep nb_frames | sed -e 's/nb_frames=//'
