892b47151ea23aa2568db5017b74d5f8aae2af9e
Luke Reding <lukereding@wireless-10-146-148-246.public.utexas.edu>
2015-12-11 14:04:04 -0600

    ensures avi files arent overwritten

--- recordAndTrack.sh
+++ recordAndTrack.sh
@@ -9,20 +9,26 @@ FPS="$3"
 # optional: sleep during the x second acclimation period
 # sleep x;
 
-# start ffmpeg
-# note here: the order of the arguments matters. Specifically, the framerate and video size arguments must be given before the -i or the line will throw an error
-# this line was tested > 50 times with the logitech camera
-ffmpeg -f avfoundation -video_size 1280x720 -framerate "$FPS" -i "Micro:none" -crf 28 -y -vcodec libx264 -t "$LENGTH" "$NAME"".avi"
+# make sure the video file doesn't already exist
+if [ ! -f "$NAME"".avi" ]; then
 
-# to find total frames: (double check this with other videos:
-#ffprobe -select_streams v -show_streams test.avi 2>/dev/null | grep nb_frames | sed -e 's/nb_frames=//'
+  # start ffmpeg
+  # note here: the order of the arguments matters. Specifically, the framerate and video size arguments must be given before the -i or the line will throw an error
+  # this line was tested > 50 times with the logitech camera
+  ffmpeg -f avfoundation -video_size 1280x720 -framerate "$FPS" -i "Micro:none" -crf 28 -vcodec libx264 -y -t "$LENGTH" "$NAME"".avi"
 
-# wait for it to finish
-wait
+  # to find total frames: (double check this with other videos:
+  #ffprobe -select_streams v -show_streams test.avi 2>/dev/null | grep nb_frames | sed -e 's/nb_frames=//'
 
-#start tracking
-echo "tracking ${NAME}"
-python realTimeTrackerShort.py -i ${NAME}.avi -n ${NAME} -f ${FPS} >> "$NAME""_log.txt"
-cat "$NAME""_log.txt" | grep "part 2" -A 5
-cat "$NAME""_log.txt" | grep "part 3" -A 5
-tail -20 "$NAME""_log.txt"
+  # wait for it to finish
+  wait
+
+  #start tracking
+  echo "tracking ${NAME}"
+  python realTimeTrackerShort.py -i ${NAME}.avi -n ${NAME} -f ${FPS} >> "$NAME""_log.txt"
+  cat "$NAME""_log.txt" | grep "part 2" -A 5
+  cat "$NAME""_log.txt" | grep "part 3" -A 5
+  tail -20 "$NAME""_log.txt"
+else
+  printf "a video file with that name already exists."
+fi
